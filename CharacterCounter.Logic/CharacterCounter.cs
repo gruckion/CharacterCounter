9aa5b74e61b15965e4378328eb53b7eaacafeed5
Stephen Rayner <stephenrrayner@dev.azure.com>
2019-09-03 18:56:09 +0100

    Created a basic character counter class and contract

---
+++ CharacterCounter.Logic/CharacterCounter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CharacterCounter.Logic
+{
+	public interface ICharacterCounter
+	{
+		int CountCharactersInSequence();
+	}
+
+
+	public class CharacterCounter : ICharacterCounter
+	{
+		public int CountCharactersInSequence()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
