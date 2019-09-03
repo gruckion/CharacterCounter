0fe198f75c63280d6ad79ebdb9d153cd22a9a1da
Stephen Rayner <stephenrrayner@dev.azure.com>
2019-09-03 18:55:37 +0100

    Shelled out test character counter test

--- CharacterCounter.Tests/UnitTest1.cs
+++ CharacterCounter.Tests/UnitTest1.cs
@@ -1,16 +1,13 @@
 using NUnit.Framework;
+using System.Linq;
 
 namespace Tests
 {
 	public class Tests
 	{
-		[SetUp]
-		public void Setup()
-		{
-		}
-
 		[Test]
-		public void Test1()
+		[TestCase("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eanean sodales justo et Enim ornare, a congue lacus commodo.", 'e')]
+		public void Test1(string hayStack, char needle)
 		{
 			Assert.Pass();
 		}
