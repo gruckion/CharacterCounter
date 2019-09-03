b812522152665decc84c993a948ac704f3c351c1
Stephen Rayner <stephen.r.rayner@gmail.com>
2019-09-03 20:29:23 +0100

    Develop (#3)
    * created a test and library project
    
    * Feature/character counter (#2)
    
    * Shelled out test character counter test
    
    * Created a basic character counter class and contract
    
    * Deleted the needed file
    
    * Asserting on total number of characters in string
    
    * Extending test coverage to the example with numbers
    
    * Covered the case for taking in numbers
    
    * Improved the counter to support string and int
    
    * Restricted to IConvertible and moved test case into single test
    
    * Added read me instructions
    
    * correcting whitespace

---
+++ CharacterCounter.Logic/OccuranceCounter.cs
@@ -0,0 +1,18 @@
+﻿namespace CharacterCounter.Logic
+{
+    using System;
+    using System.Linq;
+
+	public interface IOccuranceCounter
+	{
+		int CountCharactersInSequence<T>(T sequence, char charOfInterest) where T : IConvertible;
+	}
+
+	public class OccuranceCounter : IOccuranceCounter
+	{
+		public int CountCharactersInSequence<T>(T sequence, char charOfInterest) where T : IConvertible
+		{
+			return sequence.ToString().Count(c => c == charOfInterest);
+		}
+	}
+}
