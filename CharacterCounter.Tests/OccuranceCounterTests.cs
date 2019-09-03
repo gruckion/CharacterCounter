b53e0fcf659e7a6b6cb4baec0fd4109a845ea436
Stephen Rayner <stephen.r.rayner@gmail.com>
2019-09-03 20:27:29 +0100

    Feature/character counter (#2)
    * Shelled out test character counter test
    
    * Created a basic character counter class and contract
    
    * Deleted the needed file
    
    * Asserting on total number of characters in string
    
    * Extending test coverage to the example with numbers
    
    * Covered the case for taking in numbers
    
    * Improved the counter to support string and int
    
    * Restricted to IConvertible and moved test case into single test
    
    * Added read me instructions
    
    * correcting whitespace

---
+++ CharacterCounter.Tests/OccuranceCounterTests.cs
@@ -0,0 +1,29 @@
+namespace Tests
+{
+	using CharacterCounter.Logic;
+	using NUnit.Framework;
+	using System;
+
+    public class OccuranceCounterTests
+	{
+		private IOccuranceCounter OccuranceCounter;
+
+		[SetUp]
+		public void Setup()
+		{
+			this.OccuranceCounter = new OccuranceCounter();
+		}
+
+		[TestCase("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eanean sodales justo et Enim ornare, a congue lacus commodo.", 'e', 10)]
+		[TestCase("17272838119191929838299111", '1', 8)]
+		[TestCase(7 + 5, '1', 1)]
+		public void ShouldReturnCorrectOccuranceCountForCharacterInString<T>(
+			T sequence, char charOfInterest, int expectedOccurances) where T : IConvertible
+		{
+			var occuranceCount = this.OccuranceCounter.CountCharactersInSequence(
+				sequence, charOfInterest);
+
+			Assert.AreEqual(occuranceCount, expectedOccurances);
+		}
+	}
+}
