c16b4614e9d8917534909890bff634544dd0cb1c
Stephen Rayner <stephen.r.rayner@gmail.com>
2019-09-03 20:48:25 +0100

    Develop (#7)
    * created a test and library project
    
    * Feature/character counter (#2)
    
    * Shelled out test character counter test
    
    * Created a basic character counter class and contract
    
    * Deleted the needed file
    
    * Asserting on total number of characters in string
    
    * Extending test coverage to the example with numbers
    
    * Covered the case for taking in numbers
    
    * Improved the counter to support string and int
    
    * Restricted to IConvertible and moved test case into single test
    
    * Added read me instructions
    
    * correcting whitespace
    
    * Cleaning up (#4)
    
    * Develop (#3)
    
    * created a test and library project
    
    * Feature/character counter (#2)
    
    * Shelled out test character counter test
    
    * Created a basic character counter class and contract
    
    * Deleted the needed file
    
    * Asserting on total number of characters in string
    
    * Extending test coverage to the example with numbers
    
    * Covered the case for taking in numbers
    
    * Improved the counter to support string and int
    
    * Restricted to IConvertible and moved test case into single test
    
    * Added read me instructions
    
    * correcting whitespace
    
    * Delete Class1.cs
    
    This class was not removed from the disk and was overlooked during the review.
    
    * Removing empty UnitTest class
    
    * Whitespace is off on githib but not locally (#6)

--- CharacterCounter.Logic/OccuranceCounter.cs
+++ CharacterCounter.Logic/OccuranceCounter.cs
@@ -3,16 +3,16 @@
     using System;
     using System.Linq;
 
-	public interface IOccuranceCounter
-	{
-		int CountCharactersInSequence<T>(T sequence, char charOfInterest) where T : IConvertible;
-	}
+    public interface IOccuranceCounter
+    {
+        int CountCharactersInSequence<T>(T sequence, char charOfInterest) where T : IConvertible;
+    }
 
-	public class OccuranceCounter : IOccuranceCounter
-	{
-		public int CountCharactersInSequence<T>(T sequence, char charOfInterest) where T : IConvertible
-		{
-			return sequence.ToString().Count(c => c == charOfInterest);
-		}
-	}
+    public class OccuranceCounter : IOccuranceCounter
+    {
+        public int CountCharactersInSequence<T>(T sequence, char charOfInterest) where T : IConvertible
+        {
+            return sequence.ToString().Count(c => c == charOfInterest);
+        }
+    }
 }
